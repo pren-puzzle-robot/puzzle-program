a19d924a7ea2284a9fbc8332ea0e398d91336901
Hoiton <timscheiber87@gmail.com>
2026-05-08 17:13:26 +0200

    Rotation aktiviert

--- src/microcontroller_interface/uart_handler.py
+++ src/microcontroller_interface/uart_handler.py
@@ -88,9 +88,8 @@ class UartHandler:
     def encode_move_payload(self, command: MoveCommand) -> bytes:
         x = self.require_uint16(command.x, "x")
         y = self.require_uint16(command.y, "y")
-        # ToDo: Add rotation once Microcontroller Supports it
-        # rotation = self.require_uint16(command.rotation, "rotation")
-        return b"M" + struct.pack(f"{self.byteorder}HHH", x, y, 0)
+        rotation = self.require_uint16(command.rotation, "rotation")
+        return b"M" + struct.pack(f"{self.byteorder}HHH", x, y, rotation)   
 
     def encode_simple_command(self, command: SimpleSendCommand) -> bytes:
         return command.value.encode("ascii")
